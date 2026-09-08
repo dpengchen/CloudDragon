fdad18c6ba873f828401ca90f97409852e19d2e4
dpengchen <dpengchen@163.com>
2026-09-08 23:28:10 +0800

    服务注册发现添加网关和系统模块

--- dragon-cloud-modules/dragon-cloud-system/src/main/java/uno/rocc/dragon/cloud/DragonCloudSystemApplication.java
+++ dragon-cloud-modules/dragon-cloud-system/src/main/java/uno/rocc/dragon/cloud/DragonCloudSystemApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient 
-@SpringBootApplication 
-public class DragonCloudGatewayApplication {
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DragonCloudSystemApplication {
     public static void main(String[] args) {
         System.setProperty("nacos.server.grpc.port.offset", "1");
-        SpringApplication.run(DragonCloudGatewayApplication.class, args);
+        SpringApplication.run(DragonCloudSystemApplication.class, args);
     }
 }
